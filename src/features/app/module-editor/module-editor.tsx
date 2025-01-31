5f37a3cd5ebc1c37abe46cd672e20a8c8be5ecbf
kubalinio <jsapalawebdev@gmail.com>
2025-01-31 09:55:02 +0100

    feat(export): add first working version of export to check proper exported img

--- src/features/app/module-editor/module-editor.tsx
+++ src/features/app/module-editor/module-editor.tsx
@@ -1,11 +1,37 @@
+import { CanvasSelector, useCanvasStore } from "context/canva-store"
+import download from "downloadjs"
 import { AddImage } from "features/app/module-editor/components/add-image"
 import { AddText } from "features/app/module-editor/components/add-text"
+import * as htmlToImage from "html-to-image"
 import { Box, Button, Separator, Typography } from "ui"
+import { useShallow } from "zustand/react/shallow"
 
 import { AddBackground } from "./components/add-background"
 import { Header } from "./ui/header"
 
 const ModuleEditor = () => {
+  const { selectedElement, setSelectedElement } = useCanvasStore(
+    useShallow(CanvasSelector)
+  )
+
+  const handleImageDownload = () => {
+    setSelectedElement(null)
+
+    const node = document.getElementById("canvas")
+    if (!node) return
+
+    htmlToImage
+      .toPng(node)
+      .then(function (dataUrl) {
+        const img = new Image()
+        img.src = dataUrl
+        download(dataUrl, "my-node.png")
+      })
+      .catch(function (error) {
+        console.error("oops, something went wrong!", error)
+      })
+  }
+
   return (
     <div className='flex h-full flex-col gap-y-8'>
       <Header />
@@ -28,7 +54,9 @@ const ModuleEditor = () => {
         <AddBackground />
       </Box>
 
-      <Button className='mt-auto self-end'>Export to PNG</Button>
+      <Button className='mt-auto self-end' onClick={handleImageDownload}>
+        Export to PNG
+      </Button>
     </div>
   )
 }
